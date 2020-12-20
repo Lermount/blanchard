926d875d2d27459c97e634149839171b7b5b514a
Антон Лермонтов <Lermontov.A.A@mail.ru>
2020-12-21 00:26:50 +0300

    Правки

---
+++ Blanchard/index.js
@@ -0,0 +1,12 @@
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    spaceBetween: 50,
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+
+
+});
