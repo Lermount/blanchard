0400657adf07d92e857777b96edaa05c236cb73d
Антон Лермонтов <Lermontov.A.A@mail.ru>
2021-01-18 19:36:19 +0300

    first three blocks adaptive

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//swiper
 var mySwiper = new Swiper('.swiper-container', {
     loop: true,
     spaceBetween: 50,
@@ -11,6 +11,7 @@ var mySwiper = new Swiper('.swiper-container', {
 
 });
 
+//scroll
 new SimpleBar(document.getElementById('realizm'));
 
 new SimpleBar(document.getElementById('impresion'));
@@ -20,3 +21,51 @@ new SimpleBar(document.getElementById('postmodern'));
 new SimpleBar(document.getElementById('avangard'));
 
 new SimpleBar(document.getElementById('future'));
+
+//burger open
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#burger').addEventListener('click', function() {
+        document.querySelector('#menu').classList.toggle('is-active')
+    })
+})
+//burger close
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#closer').addEventListener('click', function() {
+        document.querySelector('#menu').classList.toggle('is-active')
+    })
+})
+
+//input-adaptive open
+function viewSearch(){
+    document.getElementById("search").style.display = "flex";
+    if(screen.width <= 768){
+        document.getElementById("logo").style.display = "none";
+        document.getElementById("burger").style.display = "none"
+    }
+    
+    if(screen.width == 320){
+        document.getElementById("search-btn").style.display = "none";
+    }else{
+        document.getElementById("logo").style.marginRight = "138px";
+    };
+    
+
+  };
+//input adaptive close
+  function closeSearch(){
+    document.getElementById("search").style.display = "none";
+    if(screen.width <= 768){
+        document.getElementById("logo").style.display = "block";
+        document.getElementById("logo").style.margin = "0 auto"
+        document.getElementById("burger").style.display = "block"
+    }
+    
+    if(screen.width >= 768){
+        document.getElementById("logo").style.margin = "0 auto";
+    }else{
+        document.getElementById("search-btn").style.display = "block";
+    };
+
+  };
+
+
