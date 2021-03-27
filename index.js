9b0acd195004d516cfbf45a7bd50eab0bb00af98
Антон Лермонтов <Lermontov.A.A@mail.ru>
2021-03-27 15:29:22 +0300

    end

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //swiper-events-mobile
-var mSwiper = new Swiper('.events-swiper',{
-    
+var mSwiper = new Swiper('.events-swiper', {
+
     spaceBetween: 50,
     pagination: {
         el: '.events-pagination',
-      },
-  
+    },
+
 });
 
 //swiper-hero
@@ -23,33 +23,33 @@ var mySwiper = new Swiper('.hero-swiper', {
 
 //swiper-gallery
 var gallerySwiper = new Swiper('.gallery-swiper', {
-    
+
     spaceBetween: 50,
     slidesPerView: 3,
     slidesPerGroup: 3,
     slidesPerColumn: 2,
     slidesPerColumnFill: 'row',
-   
+
 
     pagination: {
-      el: '.swiper-paginationg',
-      type: 'fraction',
+        el: '.swiper-paginationg',
+        type: 'fraction',
     },
 
     navigation: {
-      nextEl: '.gallery-swiper-next',
-      prevEl: '.gallery-swiper-prev',
+        nextEl: '.gallery-swiper-next',
+        prevEl: '.gallery-swiper-prev',
     },
 
     breakpoints: {
-       
+
         1420: {
             spaceBetween: 50,
             slidesPerView: 3,
             slidesPerGroup: 3,
             slidesPerColumn: 2,
             slidesPerColumnFill: 'row',
-          
+
         },
 
         351: {
@@ -58,64 +58,107 @@ var gallerySwiper = new Swiper('.gallery-swiper', {
             spaceBetween: 34,
         },
 
-        320:{
+        320: {
             slidesPerView: 1,
             slidesPerGroup: 1,
             slidesPerColumn: 1,
         },
-        
+
     },
-   
-  
+
+
 });
 
 //books-swiper
 var bookSwiper = new Swiper('.books-swiper', {
-    
+
     spaceBetween: 50,
     slidesPerView: 3,
     slidesPerGroup: 3,
-    
+
     pagination: {
-      el: '.swiper-paginationg',
-      type: 'fraction',
+        el: '.swiper-paginationg',
+        type: 'fraction',
     },
 
     navigation: {
-      nextEl: '.books-swiper-next',
-      prevEl: '.books-swiper-prev',
+        nextEl: '.books-swiper-next',
+        prevEl: '.books-swiper-prev',
     },
 
     breakpoints: {
-        1025:{
+        1025: {
             spaceBetween: 50,
             slidesPerView: 3,
             slidesPerGroup: 3,
         },
 
-        769:{
+        769: {
             spaceBetween: 50,
             slidesPerView: 2,
             slidesPerGroup: 2,
         },
 
-        689:{
+        689: {
             spaceBetween: 34,
             slidesPerView: 2,
             slidesPerGroup: 2,
         },
 
-        350:{
+        350: {
             spaceBetween: 50,
             slidesPerView: 1,
             slidesPerGroup: 1,
         }
-        
+
     },
-   
-  
+
+
 });
-  
+
+//progects-swiper
+
+var prSwiper = new Swiper('.progects-swiper', {
+
+
+    navigation: {
+        nextEl: '.progects-swiper-next',
+        prevEl: '.progects-swiper-prev',
+    },
+
+    breakpoints: {
+        1650: {
+            spaceBetween: 50,
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+        },
+
+        1024: {
+            spaceBetween: 50,
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+
+        480: {
+            spaceBetween: 34,
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+
+        320: {
+            spaceBetween: 50,
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        }
+
+
+
+    },
+
+
+});
+
+
 
 //scroll
 new SimpleBar(document.getElementById('realizm'));
@@ -129,61 +172,139 @@ new SimpleBar(document.getElementById('avangard'));
 new SimpleBar(document.getElementById('future'));
 
 //burger open
-function burger(){
+function burger() {
     document.getElementById("menu").style.display = "block";
 }
 //burger close
-function closer(){
+function closer() {
     document.getElementById("menu").style.display = "none";
 }
 
+//language
+function french() {
+    document.getElementById("catalog-main").style.display = "none";
+    document.getElementById("catalog-main2").style.display = "flex";
+}
+
+function german() {
+    document.getElementById("catalog-main").style.display = "none";
+    document.getElementById("catalog-main2").style.display = "flex";
+}
+
+function italian() {
+    document.getElementById("catalog-main").style.display = "flex";
+    document.getElementById("catalog-main2").style.display = "none";
+}
+
+function russian() {
+    document.getElementById("catalog-main").style.display = "none";
+    document.getElementById("catalog-main2").style.display = "flex";
+}
+
+function belgian() {
+    document.getElementById("catalog-main").style.display = "none";
+    document.getElementById("catalog-main2").style.display = "flex";
+}
+
+//accordion-items
+
+function accordion() {
+    document.getElementById("catalog-main-left").style.display = "none";
+    document.getElementById("catalog-main-left2").style.display = "block";
+}
+
+function Domenico() {
+    document.getElementById("catalog-main-left").style.display = "block";
+    document.getElementById("catalog-main-left2").style.display = "none";
+}
+
+
 
 //input-adaptive open
-function viewSearch(){
+function viewSearch() {
     document.getElementById("search").style.display = "flex";
-    if(screen.width <= 768){
+    if (screen.width <= 768) {
         document.getElementById("logo").style.display = "none";
         document.getElementById("burger").style.display = "none"
     }
-    
-    if(screen.width == 320){
+
+    if (screen.width == 320) {
         document.getElementById("search-btn").style.display = "none";
-    }else{
+    } else {
         document.getElementById("logo").style.marginRight = "138px";
     };
-    
 
-  };
+
+};
 //input adaptive close
-  function closeSearch(){
+function closeSearch() {
     document.getElementById("search").style.display = "none";
-    if(screen.width <= 768){
+    if (screen.width <= 768) {
         document.getElementById("logo").style.display = "block";
         document.getElementById("logo").style.margin = "0 auto"
         document.getElementById("burger").style.display = "block"
     }
-    
-    if(screen.width >= 768){
+
+    if (screen.width >= 768) {
         document.getElementById("logo").style.margin = "0 auto";
-    }else{
+    } else {
         document.getElementById("search-btn").style.display = "block";
     };
 
-  };
+};
 
-  //events-btn 
+//events-btn 
 
-  function events(){
+function events() {
     document.getElementById("card4").style.display = "block";
     document.getElementById("card5").style.display = "block";
-    if(screen.width <= 935 ){
+    document.getElementById("events-btn").style.display = "none";
+    if (screen.width <= 935) {
         document.getElementById("card3").style.display = "block";
-    }    
+    }
 
+}
+
+//yandex map
+
+
+ymaps.ready(init);
+function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        center: [55.759046, 37.607206],
+        zoom: 14.3,
+
+        
+    });
+   
+
+    
+    var myPlacemark = new ymaps.Placemark([55.758463, 37.601079], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/point.svg',
+        iconImageSize: [20, 20],
+        iconImageOffset: [-3, -42]
+    });
+    myMap.geoObjects.add(myPlacemark);
+    myMap.controls.remove('geolocationControl');
+    myMap.controls.remove('zoomControl');
+    myMap.controls.remove('trafficControl');
+    myMap.controls.remove('typeSelector');
+    myMap.controls.remove('fullscreenControl');
+
+
+    
+
+    
 }
 
 
 
- 
+
+
+
+
+
 
 
